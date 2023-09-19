15c4824bba1b89e3b194f9e2b913d34df2d06cd0
Erion Berdynaj <erion.berdyna@hotmail.com>
2023-09-19 20:13:55 +0200

    Implementimi ne frontend

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <abp-loader-bar></abp-loader-bar>
+    <abp-dynamic-layout></abp-dynamic-layout>
+  `,
 })
-export class AppComponent {
-  title = 'angular';
-}
+export class AppComponent {}
